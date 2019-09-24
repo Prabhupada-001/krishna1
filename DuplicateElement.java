938b2004580ce1ba6a9b25445aa50db56ec499fd
Prabhupada-001 <54739657+Prabhupada-001@users.noreply.github.com>
2019-09-24 20:28:51 +0530

    Update DuplicateElement.java

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -5,7 +5,8 @@
             System.out.println("Duplicate elements in given array: ");  
             //Searches for duplicate element  
             for(int i = 0; i < arr.length; i++) {  
-                for(int j = i + 1; j < arr.length; j++) {  
+                for(int j = i + 1; j < arr.length; j++) 
+                for(int i = 0; i < arr.length; i++){  
                     if(arr[i] == arr[j])  
                         System.out.println(arr[j]);  
                 }  
